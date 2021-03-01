b3dca6d25dcdc6e44b34ff880d36b0add095d987
Tim Quimbo <tquimbo1@gmail.com>
2021-03-01 01:28:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-"Hey I am looking for vowels!".match(/\w+[aeiou]/)
+"Hey I am looking for vowels!".scan(/\w+[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
